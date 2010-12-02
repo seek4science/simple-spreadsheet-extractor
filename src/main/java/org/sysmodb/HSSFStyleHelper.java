3da8ad88292e163fba62791d5d4b6cd874149264
Finn <finn@Finn1.(none)>
2010-12-02 20:48:24 +0800

    Added styles and layout data to XML output

---
+++ src/main/java/org/sysmodb/HSSFStyleHelper.java
@@ -0,0 +1,76 @@
+package org.sysmodb;
+
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFPalette;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.dom4j.Element;
+
+/**
+ *
+ * @author Finn
+ */
+
+public class HSSFStyleHelper implements StyleHelper{
+
+    private static HSSFWorkbook workbook;
+    private static HSSFPalette palette;
+
+    private static final HSSFColor HSSF_AUTO = new HSSFColor.AUTOMATIC();
+
+
+    public HSSFStyleHelper(HSSFWorkbook wb)
+    {
+	workbook = wb;
+	palette = wb.getCustomPalette();
+    }
+
+    public String getBGColour(CellStyle style)
+    {
+	return getRGBString(style.getFillForegroundColor());
+    }
+
+    public void setFontProperties(CellStyle style, Element element)
+    {
+	HSSFCellStyle newStyle = (HSSFCellStyle) style;
+	HSSFFont font = newStyle.getFont(workbook);
+	if(font.getBoldweight() == HSSFFont.BOLDWEIGHT_BOLD)
+	    element.addElement("font-weight").addText("bold");
+	if(font.getItalic())
+	    element.addElement("font-style").addText("italics");
+	if(font.getUnderline() != HSSFFont.U_NONE)
+	    element.addElement("text-decoration").addText("underline");
+	//Ignore same-ish defaults
+	if(font.getFontHeightInPoints() != 10 && font.getFontHeightInPoints() != 11)
+	    element.addElement("font-size").addText(String.valueOf(font.getFontHeightInPoints() + "pt"));
+	//Arial is default for Excel, Calibri is default for OO
+	if(!font.getFontName().equals("Arial") && !font.getFontName().equals("Calibri"))
+	    element.addElement("font-family").addText(font.getFontName());
+	if(font.getColor() != HSSFFont.COLOR_NORMAL)
+	    element.addElement("color").addText(getRGBString(font.getColor()));
+    }
+
+    private String getRGBString(short index)
+    {
+	String string = null;
+	HSSFColor color = palette.getColor(index);
+
+	if (index == HSSF_AUTO.getIndex() || color == null)
+	{
+
+	}
+	else
+	{
+	    short[] rgb = color.getTriplet();
+	    string = "#";
+	    string += Integer.toHexString((int) rgb[0]);
+	    string += Integer.toHexString((int) rgb[1]);
+	    string += Integer.toHexString((int) rgb[2]);
+	}
+
+
+	return string;
+    }
+}
---
+++ src/main/java/org/sysmodb/StyleGenerator.java
@@ -0,0 +1,57 @@
+package org.sysmodb;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
+import org.dom4j.Element;
+
+/**
+ *
+ * @author Finn
+ */
+public abstract class StyleGenerator {
+    private static final Map<Short, String> BORDERS = createBorderMap();
+
+    private static Map<Short, String> createBorderMap() {
+        Map<Short, String> result = new HashMap<Short, String>();
+        result.put(CellStyle.BORDER_DASHED, "dashed 1pt");
+	result.put(CellStyle.BORDER_DASH_DOT, "dashed 1pt");
+	result.put(CellStyle.BORDER_DASH_DOT_DOT, "dashed 1pt");
+	result.put(CellStyle.BORDER_DOTTED, "dotted 1pt");
+	result.put(CellStyle.BORDER_DOUBLE, "double 3pt");
+	result.put(CellStyle.BORDER_HAIR, "solid 1pt");
+	result.put(CellStyle.BORDER_MEDIUM, "2pt solid");
+        result.put(CellStyle.BORDER_MEDIUM_DASHED, "2pt dashed");
+	result.put(CellStyle.BORDER_MEDIUM_DASH_DOT, "2pt dashed");
+	result.put(CellStyle.BORDER_MEDIUM_DASH_DOT_DOT, "2pt dashed");
+	result.put(CellStyle.BORDER_NONE, "none");
+	result.put(CellStyle.BORDER_SLANTED_DASH_DOT, "dashed 2pt");
+	result.put(CellStyle.BORDER_THICK, "solid 3pt");
+	result.put(CellStyle.BORDER_THIN, "dashed 1pt");
+
+        return Collections.unmodifiableMap(result);
+    }
+
+    public static void createStyle(CellStyle style, Element element, StyleHelper helper)
+    {
+	//Border properties
+	String border = "none";
+	if((border = BORDERS.get(style.getBorderTop())) != "none")
+	    element.addElement("border-top").addText(border);
+	if((border = BORDERS.get(style.getBorderBottom())) != "none")
+	    element.addElement("border-bottom").addText(border);
+	if((border = BORDERS.get(style.getBorderLeft())) != "none")
+	    element.addElement("border-left").addText(border);
+	if((border = BORDERS.get(style.getBorderRight())) != "none")
+	    element.addElement("border-right").addText(border);
+
+	//Background/fill colour
+	String backgroundColour;
+	if((backgroundColour = helper.getBGColour(style)) != null)
+	    element.addElement("background-color").addText(backgroundColour);
+
+	//Font properties
+	helper.setFontProperties(style, element);
+    }
+}
