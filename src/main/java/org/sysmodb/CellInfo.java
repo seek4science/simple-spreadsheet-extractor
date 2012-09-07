c0939db7e3951722ade55498b2190c215c6fc663
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-09-07 11:58:04 +0100

    uses the cached value and type for a formula cell, rather than reevaluating the formula

--- src/main/java/org/sysmodb/CellInfo.java
+++ src/main/java/org/sysmodb/CellInfo.java
@@ -3,11 +3,8 @@ package org.sysmodb;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.poi.ss.formula.eval.NotImplementedException;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellValue;
 import org.apache.poi.ss.usermodel.DateUtil;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Workbook;
 
 public class CellInfo {
@@ -25,66 +22,39 @@ public class CellInfo {
 			type = "blank";
 			formula = null;
 		} else {
-			switch (cell.getCellType()) {
-			case Cell.CELL_TYPE_BLANK:
-				value = "";
-				type = "blank";
-				break;
-			case Cell.CELL_TYPE_BOOLEAN:
-				value = String.valueOf(cell.getBooleanCellValue());
-				type = "boolean";
-				break;
-			case Cell.CELL_TYPE_NUMERIC:
-				if (DateUtil.isCellDateFormatted(cell)) {
-					type = "datetime";
-					Date dateCellValue = cell.getDateCellValue();
-					value = dateFormatter.format(dateCellValue);
-				} else {
-					value = String.valueOf(cell.getNumericCellValue());
-					type = "numeric";
-				}
-				break;
-			case Cell.CELL_TYPE_STRING:
-				value = cell.getStringCellValue();
-				type = "string";
-				break;
-			case Cell.CELL_TYPE_FORMULA:
-
-				FormulaEvaluator evaluator = workbook.getCreationHelper()
-						.createFormulaEvaluator();				
-				CellValue cellValue;
-				formula = cell.getCellFormula();				
-				try {
-					cellValue = evaluator.evaluate(cell);
-					value = cellValue.formatAsString();					
-					switch (cellValue.getCellType()) {
-					case Cell.CELL_TYPE_BOOLEAN:
-						type = "boolean";
-						break;
-					case Cell.CELL_TYPE_STRING:
-						type = "string";
-						break;
-					case Cell.CELL_TYPE_NUMERIC:
-						type = "numeric";
-						if (DateUtil.isCellDateFormatted(cell)) {
-							type = "datetime";
-							value = dateFormatter.format(DateUtil
-									.getJavaDate(cellValue.getNumberValue()));
-						}
-						break;
-					}
-				}
-				catch(NotImplementedException e) {
-					type="error";
-					value=e.getMessage();
-				}
-				catch(RuntimeException e) {
-					type="error";
-					value=e.getMessage();
-				}
-				
-				break;
+			readCellValueAndType(cell.getCellType(),cell);			
+		}
+	}
+	
+	private void readCellValueAndType(int cellType,Cell cell) {
+		switch (cellType) {
+		case Cell.CELL_TYPE_BLANK:
+			value = "";
+			type = "blank";
+			break;
+		case Cell.CELL_TYPE_BOOLEAN:
+			value = String.valueOf(cell.getBooleanCellValue());
+			type = "boolean";
+			break;
+		case Cell.CELL_TYPE_NUMERIC:
+			if (DateUtil.isCellDateFormatted(cell)) {
+				type = "datetime";
+				Date dateCellValue = cell.getDateCellValue();
+				value = dateFormatter.format(dateCellValue);
+			} else {
+				value = String.valueOf(cell.getNumericCellValue());
+				type = "numeric";
 			}
+			break;
+		case Cell.CELL_TYPE_STRING:
+			value = cell.getStringCellValue();
+			type = "string";
+			break;
+		case Cell.CELL_TYPE_FORMULA:			
+			formula = cell.getCellFormula();
+			int resultCellType = cell.getCachedFormulaResultType();
+			readCellValueAndType(resultCellType,cell);			
+			break;
 		}
 	}
 }
