41c05991146b612b7183fdcf825c137785e90318
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-04-10 14:32:51 +0100

    added copyright headers

--- src/main/java/org/sysmodb/CellInfo.java
+++ src/main/java/org/sysmodb/CellInfo.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2013, University of Manchester
+ *  
+ * Licensed under the New BSD License. 
+ * Please see LICENSE file that is distributed with the source code
+ ******************************************************************************/
 package org.sysmodb;
 
 import java.text.SimpleDateFormat;
